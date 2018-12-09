099b458ae65f85715cffd905820793872e60ca75
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-12-09 10:02:51 +0100

    Add binned wages

--- normalize-country.js
+++ normalize-country.js
@@ -1,76 +1,185 @@
-const countries = require('country-data/data/countries.json'),
-  _ = require('lodash');
+const countries = require("country-data/data/countries.json"),
+  _ = require("lodash");
 
 const summary = {
   success: 0,
-  failure: 0
+  failure: 0,
+  failures: {}
 };
 
-function normalizeCountry (input) {
+function normalizeCountry(input) {
   let toReturn;
 
-  let success = (returnValue) => {
-    toReturn = returnValue;
+  let success = value => {
+    toReturn = value;
     summary.success += 1;
-  }
+  };
+
+  let failure = value => {
+    summary.failure += 1;
+
+    if (summary.failures[value]) {
+      summary.failures[value]++;
+    } else {
+      summary.failures[value] = 1;
+    }
+  };
 
   // name match
-  let matchedCountry = _.find(countries, _.matchesProperty('name', input));
-  if(matchedCountry) {
+  let matchedCountry = _.find(countries, _.matchesProperty("name", input));
+  if (matchedCountry) {
     success(matchedCountry.alpha3);
     return toReturn;
   }
 
   // match code alpha3
-  matchedCountry = _.find(countries, _.matchesProperty('alpha3', input));
-  if(matchedCountry) {
+  matchedCountry = _.find(countries, _.matchesProperty("alpha3", input));
+  if (matchedCountry) {
     success(matchedCountry.alpha3);
     return toReturn;
   }
 
   // match code alpha2
-  matchedCountry = _.find(countries, _.matchesProperty('alpha2', input));
-  if(matchedCountry) {
+  matchedCountry = _.find(countries, _.matchesProperty("alpha2", input));
+  if (matchedCountry) {
     success(matchedCountry.alpha3);
     return toReturn;
   }
 
   // manual mapping
-  switch(input) {
-    case 'SPain':
-      success('ESP')
+  switch (input) {
+    case "SPain":
+    case "spain":
+      success("ESP");
       break;
-    case 'Russia':
-      success('RUS')
+    case "México":
+      success("MEX");
       break;
-    case 'United States of America':
-    case 'Usa':
-      success('USA')
+    case "germany":
+    case 'Germany ':
+      success("DEU");
       break;
-    case 'Brasil':
-    case 'brazil':
-      success('BRA')
+    case "canada":
+    case "Canada ":
+      success("CAN");
       break;
-    case 'Bolivia':
-      success('BOL')
+    case "Czechia":
+    case "Czech republic":
+      success("CZE");
       break;
-    case 'United Kingdom':
-    case 'England':
-    case 'Scotland':
-      success('GBR')
+    case "Australia":
+    case "Australia ":
+    case "australia ":
+    case "australia":
+      success("AUS");
       break;
-    case 'South Korea':
-    case 'Korea':
-    case 'Republic of Korea':
-    case 'Korea':
-      success('KOR');
+    case "france":
+    case "France ":
+      success("FRA");
+      break;
+    case "Russia":
+    case "Russia ":
+    case "russia":
+    case "russia ":
+      success("RUS");
+      break;
+    case "india":
+    case "India ":
+    case "india ":
+      success("IND");
+      break;
+    case 'Iran':
+      success("IRN");
+      break;
+    case 'italy':
+      success("ITA");
+      break;
+    case "United States of America":
+    case "Usa":
+    case "usa":
+    case "Us":
+    case "us":
+    case "United States ":
+    case "united states":
+    case "United states":
+    case "USA ":
+    case  'U.S.A.':
+      success("USA");
+      break;
+    case "Venezuela":
+      success("VEN");
+      break;
+    case "Brasil":
+    case "brazil":
+      success("BRA");
+      break;
+    case "Bolivia":
+      success("BOL");
+      break;
+    case "United Kingdom":
+    case "united kingdom":
+    case "United kingdom":
+    case "United Kingdom ":
+    case "England":
+    case "Scotland":
+    case "Uk":
+    case "uk":
+      success("GBR");
+      break;
+    case "South Korea":
+    case "Korea":
+    case "Republic of Korea":
+    case "Korea":
+      success("KOR");
+      break;
+    case "The Netherlands":
+    case "Netherlands ":
+    case "netherlands":
+    case "the Netherlands":
+      success("NLD");
+      break;
+    case "sweden":
+    case "Sweden ":
+      success("SWE");
+      break;
+    case "Vietnam":
+      success("VNM");
+      break;
+    case "indonesia":
+      success("IDN");
+      break;
+    case "Ukraine ":
+      success("UKR");
+      break;
+    case "belgium":
+      success("BEL");
+      break;
+    case "Switzerland ":
+      success("CHE");
+      break;
+    case "Denmark ":
+      success("DNK");
+      break;
+    case "israel":
+      success("ISR");
+      break;
+    case "Perú":
+      success("PER");
+      break;
+    case 'Bosnia and Herzegovina':
+      success("BIH");
+      break;
+    case 'Macedonia':
+      success("MKD");
+      break;
+    case 'poland':
+      success("POL");
       break;
     default:
-      console.log(input);
-      summary.failure += 1;
+      failure(input);
   }
 
   return input;
-};
+}
 
-module.exports = {normalizeCountry, summary};
+module.exports = { normalizeCountry, summary };
--- process-js.js
+++ process-js.js
@@ -8,39 +8,45 @@ const _ = require('lodash'),
     .map(entry => {
       return {
         country: normalizeCountry(entry.country),
-        salary: entry.salary
+        salary: entry.salary,
       };
     }),
-  mappedSalaries = data
-    .map(entry => {
-      switch (entry.salary) {
-        case 'I work for free :(':
-          entry.salary = 0;
-          break;
-        case '$0-$10k':
-          entry.salary = 5000;
-          break;
-        case '$10k-$30k':
-          entry.salary = 20000;
-          break;
-        case '$30k-$50k':
-          entry.salary = 40000;
-          break;
-        case '$50k-$100k':
-          entry.salary = 75000;
-          break;
-        case '$100k-$200k':
-          entry.salary = 150000;
-          break;
-        case '$200k+':
-          entry.salary = 250000;
-      }
-      return entry;
-    }),
+  mappedSalaries = data.map(entry => {
+    let salary;
+
+    switch (entry.salary) {
+      case 'I work for free :(':
+        salary = 0;
+        break;
+      case '$0-$10k':
+        salary = 5000;
+        break;
+      case '$10k-$30k':
+        salary = 20000;
+        break;
+      case '$30k-$50k':
+        salary = 40000;
+        break;
+      case '$50k-$100k':
+        salary = 75000;
+        break;
+      case '$100k-$200k':
+        salary = 150000;
+        break;
+      case '$200k+':
+        salary = 250000;
+    }
+    return {
+      salary,
+      country: entry.country
+    };
+  }),
   combined = _.reduce(
     mappedSalaries,
     function(result, value) {
-      (result[value.country] || (result[value.country] = [])).push(value.salary);
+      (result[value.country] || (result[value.country] = [])).push(
+        value.salary,
+      );
       return result;
     },
     {},
@@ -50,9 +56,38 @@ const value = _.mapValues(combined, values => {
   return _.mean(values);
 });
 
-console.log('success', summary.success, 'vs failures', summary.failure)
+const failures = _.chain(summary.failures)
+  .map((count, key) => {
+    return {count, key};
+  })
+  .sortBy(entry => entry.count)
+  .takeRight(5)
+  .value();
 
-fs.writeFileSync(
-  'js-country-wage.json',
-  JSON.stringify(value),
-  'utf8');
+console.log(failures);
+
+console.log('success', summary.success, 'vs failures', summary.failure);
+
+const salaries = _.reduce(
+  data,
+  function(result, value) {
+    if (result[value.country]) {
+      if(result[value.country][value.salary]) {
+      result[value.country][value.salary] += 1;
+      } else {
+
+      result[value.country][value.salary] = 1;
+      }
+    } else {
+      result[value.country] = {};
+      result[value.country][value.salary] = 1;
+    }
+
+    return result;
+  },
+  {},
+);
+
+fs.writeFileSync('js-binned-wages.json', JSON.stringify(value), 'utf8');
+
+fs.writeFileSync('js-country-wage.json', JSON.stringify(value), 'utf8');
